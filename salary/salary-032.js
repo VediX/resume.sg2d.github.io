95c65efacf600421938247c0cc2ab4a30f053fd7
ilya <ilya@ilya-u-main>
2022-03-01 10:31:51 +0300

    salary 01.03.2022

--- salary/salary-032.js
+++ salary/salary-032.js
@@ -143,7 +143,7 @@ class Salary extends SGModelView {
 		S: "sg2d",
 	};
 	
-	static HOUR_RATE_BASE = 2990;
+	static HOUR_RATE_BASE = 2500;
 	static HOUR_RATE_MIN = 1500;
 	static RELOCATION_MONTH_MIN = 500000;
 	static RELOCATION_RATE_MIN = 500000/80;
