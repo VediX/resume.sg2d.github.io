7db5edcde1cc8c9beb1a7eaf3c258424b977522e
ilya <ilya@ilya-u-main>
2024-04-26 10:26:23 +0300

    fixes (markdown)

--- res/main.js
+++ res/main.js
@@ -1,5 +1,5 @@
 fetch("/README.md").then(async function(response) {
-	var converter = new showdown.Converter();
+	var converter = new showdown.Converter({ simplifiedAutoLink: true, ghCompatibleHeaderId: true});
 	var text = await response.text();
 	var html = converter.makeHtml(text);
 	document.querySelector("#markdown_content").innerHTML = html;
